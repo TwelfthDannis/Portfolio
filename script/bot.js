10d24ef70f577cd81cf3c04835a45c98129b70b1
Danni <89629702+TwelfthDannis@users.noreply.github.com>
2024-02-23 18:23:23 +0700

    test bot

--- script/bot.js
+++ script/bot.js
@@ -1,9 +1,11 @@
 const {Telegraf} = require("telegraf");
+const {createServer} = require("http");
 
 const bot = new Telegraf("6271272669:AAFvdMfl_iTk-_VY-2G0nU_bBtTylsnZFhY");
 
 bot.start((ctx) => ctx.reply('Welcome'));
 
-bot.launch()
-    .then(() => console.log('Bot started'))
-    .catch((err) => console.error(err));
+const server = createServer(bot.webhookCallback('/secret-path'));
+server.listen(63342, () => {
+    console.log('Server is running');
+});
