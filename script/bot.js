4c8a8f29487447d7aabe13acddfeda08c5aabe21
Danni <89629702+TwelfthDannis@users.noreply.github.com>
2024-02-23 18:17:41 +0700

    test bot

--- script/bot.js
+++ script/bot.js
@@ -1,11 +1,9 @@
-const { Telegraf } = require("telegraf");
+const {Telegraf} = require("telegraf");
 
 const bot = new Telegraf("6271272669:AAFvdMfl_iTk-_VY-2G0nU_bBtTylsnZFhY");
 
-function Telegram(){
-    bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => ctx.reply('Welcome'));
 
-    bot.launch()
-        .then(() => console.log('Bot started'))
-        .catch((err) => console.error(err));
-}
+bot.launch()
+    .then(() => console.log('Bot started'))
+    .catch((err) => console.error(err));
